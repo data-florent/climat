c1a32f1ecd8e408b17f30fcba15ae5fb0b6e7b69
data-florent <161859151+data-florent@users.noreply.github.com>
2024-03-02 11:30:18 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+
+df = "à définir"
+
+st.title("Projet data sur le changement climatique et les catastrophes naturelles")
+st.sidebar.title("Sommaire")
+pages=["Exploration", "DataVizualization", "Modélisation"]
+page=st.sidebar.radio("Aller vers", pages)
+
+if page == pages[0] : 
+  st.write("### Introduction")
+
+if page == pages[1] : 
+  st.write("### DataVizualization")
+
+if page == pages[2] : 
+  st.write("### Modélisation")
