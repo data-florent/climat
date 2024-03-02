97a047d2bfa2154f1a96bcf32bc93901c3135ce3
data-florent <161859151+data-florent@users.noreply.github.com>
2024-03-02 12:16:28 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -2,9 +2,21 @@ import streamlit as st
 #import pandas as pd
 #import numpy as np
 
+### Importation des dataframes
+
 df = "à définir"
 
-st.title("Projet data sur le changement climatique et les catastrophes naturelles")
+### Header
+image_iceberg = Image.open('images/iceberg.jpg')
+st.image(image_iceberg, use_column_width=True, width=500)
+st.markdown(
+"<h2 style='text-align: center'>"
+"<strong>Le changement climatique</strong>"
+"<br><span style='font-size: smaller'>et les catastrophes naturelles</span>"
+"</h2>"
+, unsafe_allow_html=True)
+
+
 st.sidebar.title("Sommaire")
 pages=["Présentation du projet", "DataVizualization", "Modélisation"]
 page=st.sidebar.radio("Aller vers", pages)
