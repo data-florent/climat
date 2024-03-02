d6a893322bc12ccf8a0a7495527904cb7e809ca9
data-florent <161859151+data-florent@users.noreply.github.com>
2024-03-02 12:38:16 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
+### Importation des librairies ###
 import streamlit as st
-#import pandas as pd
-#import numpy as np
 
-### Importation des dataframes
+### Importation des dataframes ###
 
 df = "à définir"
 
-### Header
-#image_iceberg = Image.open('images/iceberg.jpg')
+### Création des pages ###
+pages=["Présentation du projet", "DataVizualization", "Modélisation"]
+
+### Header ###
 st.image('images/iceberg.jpg', use_column_width=True, width=500)
 st.markdown(
 "<h2 style='text-align: center'>"
@@ -16,11 +17,32 @@ st.markdown(
 "</h2>"
 , unsafe_allow_html=True)
 
-
+### Menu latéral ###
 st.sidebar.title("Sommaire")
-pages=["Présentation du projet", "DataVizualization", "Modélisation"]
-page=st.sidebar.radio("Aller vers", pages)
+page=st.sidebar.radio(pages)
+st.sidebar.info(
+"Auteurs : "
+"\n"
+"Laura "
+"[linkedIn](https://www.linkedin.com/), "
+"Marion "
+"[linkedIn](https://www.linkedin.com/), "
+"Florent "
+"[linkedIn](https://www.linkedin.com/)"
+"Romain "
+"[linkedIn](https://www.linkedin.com/)"
+"\n\n"
+"Projet fil rouge Data Analyst avril 2024, "
+"[DataScientest](https://datascientest.com/)"
+"\n\n"
+"Données :"
+"\n"
+"[NASA](https://data.giss.nasa.gov/gistemp/), "
+"[OWID](https://github.com/owid/co2-data), "
+"[EMDAT](https://www.emdat.be/)"
+)
 
+### Contenu des pages ###
 if page == pages[0] : 
   st.write("### Présentation du projet")
 
