a2cd156830b2a5b30ab072a442dfe2b52953f4be
data-florent <161859151+data-florent@users.noreply.github.com>
2024-03-03 10:10:15 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-#import plotly.figure_factory as ff
-#import plotly.express as px
+import seaborn as sns
+import plotly.express as px
 
 ### Importation des dataframes ###
 
