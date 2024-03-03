e58cff9d10924f03498fa883f279e4ea6d43dbf3
data-florent <161859151+data-florent@users.noreply.github.com>
2024-03-03 10:35:09 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ st.markdown(
 ### Menu latéral et création des pages ###
 st.sidebar.title("Changement climatique & catastrophes naturelles")
 st.sidebar.subheader("Sommaire") 
-pages=["Présentation du projet", "DataVizualization", "Modélisation"]
+pages=["Présentation du projet", "Nettoyage des données", "Visualisations", "Modélisation"]
 page=st.sidebar.radio("",pages)
 st.sidebar.info(
 "Auteurs du projet :"
@@ -47,19 +47,36 @@ if page == pages[0] :
   st.write("### Présentation du projet")
 
 if page == pages[1] : 
-  st.write("### DataVizualization")
-  st.dataframe(country_df_OWID_CO_CLEAN.head())
-  sorted_country_df_OWID_CO_CLEAN = country_df_OWID_CO_CLEAN.sort_values(by=['year'], ascending=True)
-  sorted_country_df_OWID_CO_CLEAN = sorted_country_df_OWID_CO_CLEAN.loc[sorted_country_df_OWID_CO_CLEAN['year']>=1851]
-  fig = px.choropleth(sorted_country_df_OWID_CO_CLEAN,
+  st.write("### Nettoyage des données")
+
+if page == pages[2] : 
+  st.header("Visualisations")
+  viz1 = "Evolutions du climat"
+	viz2 = "Emissions de gaz à effet de serre"
+	viz3 = "Catastrophes naturelles"
+	dataviz_page = st.radio("", (viz1, viz2, viz3))
+  
+  #Evolutions du climat
+	if dataviz_page == viz1:
+		st.subheader("Evolutions du climat")
+  
+  #Emissions de gaz à effet de serre
+	if dataviz_page == viz2:
+		st.dataframe(country_df_OWID_CO_CLEAN.head())
+    sorted_country_df_OWID_CO_CLEAN = country_df_OWID_CO_CLEAN.sort_values(by=['year'], ascending=True)
+    sorted_country_df_OWID_CO_CLEAN = sorted_country_df_OWID_CO_CLEAN.loc[sorted_country_df_OWID_CO_CLEAN['year']>=1851]
+    fig = px.choropleth(sorted_country_df_OWID_CO_CLEAN,
                     locationmode='country names', locations='country',
                     color='share_of_temperature_change_from_ghg',
                     color_continuous_scale=px.colors.sequential.Bluered,
                     range_color=[0,15], # permet de garder la même échelle pour toutes les années
                     hover_name='country', projection='natural earth', animation_frame='year',
                     title='Part (en %) de contribution au réchauffement climatique, basée sur les émissions de GES')
-  st.plotly_chart(fig, use_container_width=False)
+    st.plotly_chart(fig, use_container_width=True)
 
+  #Catastrophes naturelles
+	if dataviz_page == viz3:
+		st.write("Catastrophes naturelles")
 
-if page == pages[2] : 
+if page == pages[3] : 
   st.write("### Modélisation")
