d075464ca04f4e4894382a97c113fbb5e29f5f66
data-florent <161859151+data-florent@users.noreply.github.com>
2024-04-06 11:25:32 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -137,7 +137,7 @@ if page == pages[2] :
 
     df_repartition = world_df_OWID_CO_CLEAN[world_df_OWID_CO_CLEAN.year >= 2000]
     df_repartition.rename(columns={"cement_co2": "Ciment", "coal_co2": "Charbon", "flaring_co2": "Torchage", "gas_co2": "Gaz", "oil_co2": "Pétrole"}, inplace=True)
-    fig = plt.figure()
+    fig = plt.figure(figsize = (5, 3))
     df_repartition[['year', 'Ciment', 'Charbon', 'Torchage', 'Gaz', 'Pétrole']].plot(x='year', kind='bar', stacked=True, linewidth=15)
     plt.title("Répartition de l'origine des gaz à effet de serre entre 2000 et 2021")
     plt.xlabel("Années")
