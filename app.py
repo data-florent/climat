52dc066e68398437c86fb3b726969cf7e31026a4
Marion <55880449+mariondepery@users.noreply.github.com>
2024-04-17 08:53:00 +0200

    Update app.py

--- app.py
+++ app.py
@@ -170,11 +170,6 @@ if page == pages[1] :
    # remplacer les NAs
   df_OWID_CO_CLEAN = df_OWID_CO['iso_code'].fillna('AUTRE')
   df_OWID_CO_CLEAN = df_OWID_CO.fillna(0)
-  
-  # Trier le DataFrame par année etre sur que les valeures sont dans l'ordre croissant
-  df = df.sort_values(by='year')
-  # Remplir les valeurs nulles dans la colonne 'population' avec la valeur de la cellule du dessus
-  df['population'] = df['population'].replace(0, method='ffill')  
   ''', language='python')
 #text	
   st.write("<p style='text-align: justify'>"
